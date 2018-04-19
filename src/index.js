561fb3475dac35c3ee8d56c7c9fb0c61f5ea0d05
Jarvis Ao Ieong <kinua1230@gmail.com>
2018-04-19 18:03:19 +0800

    cancel proptype

--- src/index.js
+++ src/index.js
@@ -1,63 +1,53 @@
-import React, {Component, PropTypes} from 'react';
-import createTruncate from 'truncate-dom';
-import stripTags from 'striptags';
+import React, { Component } from 'react'
+import createTruncate from 'truncate-dom'
+import stripTags from 'striptags'
 
 export default class Truncate extends Component {
-
-  static propTypes = {
-    html: PropTypes.string,
-    line: PropTypes.number,
-    truncateText: PropTypes.string,
-    isStripTags: PropTypes.bool,
-    allowedTags: PropTypes.string,
-  };
-
   static defaultProps = {
     html: '',
     line: 1,
     truncateText: '...',
     isStripTags: true,
     allowedTags: '<em>',
-  };
+  }
 
   componentDidMount() {
-    this.truncate = createTruncate(this.refs.scope);
-    this.forceUpdate();
-    window.addEventListener('resize', this.handleResize);
+    this.truncate = createTruncate(this.refs.scope)
+    this.forceUpdate()
+    window.addEventListener('resize', this.handleResize)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener('resize', this.handleResize)
   }
 
   handleResize = () => {
-    this.forceUpdate();
-  };
+    this.forceUpdate()
+  }
 
   getRenderHtml() {
-    const {html, line, truncateText, isStripTags, allowedTags} = this.props;
-    let stripedHtml = html;
+    const { html, line, truncateText, isStripTags, allowedTags } = this.props
+    let stripedHtml = html
 
     if (isStripTags) {
-      stripedHtml = stripTags(html, allowedTags);
+      stripedHtml = stripTags(html, allowedTags)
     }
 
-    return this.truncate(stripedHtml, line, truncateText);
+    return this.truncate(stripedHtml, line, truncateText)
   }
 
   render() {
-    let html = '';
+    let html = ''
     if (this.refs.scope) {
-      html = this.getRenderHtml();
+      html = this.getRenderHtml()
     }
 
     return (
       <div
         {...this.props}
         ref="scope"
-        dangerouslySetInnerHTML={{__html: html}}
+        dangerouslySetInnerHTML={{ __html: html }}
       />
-    );
+    )
   }
-
 }
